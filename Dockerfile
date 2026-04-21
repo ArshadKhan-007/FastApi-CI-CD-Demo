89c70302568dc9fa48473e9b64204463666dd02e
Arshad Khan <Arshadkhan627@outlook.com>
2026-04-21 21:42:07 +0530

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.12-alpine
+
+WORKDIR /app
+
+COPY . /app/
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+EXPOSE 8000
+
+ENTRYPOINT ["uvicorn", "main:app"]
+
+CMD ["--host", "0.0.0.0", "--port", "8000"]
+
---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, BackgroundTasks
+from pydantic import BaseModel
+import time
+
+app = FastAPI()
+db = []
+
+class Task(BaseModel):
+    name: str
+    priority: int
+
+def heavy_processing(task_name: str):
+    # Simulates a background job
+    print(f"Starting heavy processing for: {task_name}")
+    time.sleep(5)
+    print(f"Finished processing: {task_name}")
+
+@app.get("/")
+def read_root():
+    return {"status": "System Online", "tasks_count": len(db)}
+
+@app.post("/tasks")
+def create_task(task: Task, background_tasks: BackgroundTasks):
+    db.append(task)
+    background_tasks.add_task(heavy_processing, task.name)
+    return {"message": "Task received and processing in background", "task": task}
+
+@app.get("/analytics")
+def get_analytics():
+    high_priority = [t for t in db if t.priority > 5]
+    return {
+        "total_tasks": len(db),
+        "high_priority_count": len(high_priority)
+    }
+
+@app.get("/health")
+def health_check():
+    return {"status" : "Green..!Good to go.."}
+
+
+
+
+
+    
